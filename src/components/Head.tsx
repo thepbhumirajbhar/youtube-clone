3d7cf1cd639f10083f999f4875e8760c3411c585
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-05-08 21:15:58 +0530

    feat: hamburger icon added(sidebar btn)

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,15 +2,18 @@ const Head = () => {
   return (
     <div>
       <div>
-        left section
+        {/* left section */}
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1280px-Hamburger_icon.svg.png"
+          alt="sidebar menu"/>
       </div>
 
       <div>
-        middle section
+        {/* middle section */}
       </div>
 
       <div>
-        right section
+        {/* right section */}
       </div>
     </div>
   )
