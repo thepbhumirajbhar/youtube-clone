ebd30af37d721383488669598ceb6c17e7b17b54
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-05-08 21:20:10 +0530

    added youtube logo + aligned(flex)

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,11 +1,17 @@
 const Head = () => {
   return (
     <div>
-      <div>
+      <div className="flex gap-5">
         {/* left section */}
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1280px-Hamburger_icon.svg.png"
-          alt="sidebar menu"/>
+          alt="sidebar menu"
+          className="w-7"/>
+
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/20/YouTube_2024.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original"
+            alt="youtube-logo"
+            className="w-24"/>
       </div>
 
       <div>
