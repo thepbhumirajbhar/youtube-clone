e8ee34b7c273cc7f489ea150b56d29f0b1e985ee
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-05-12 23:54:18 +0530

    ui: added sections in sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,29 @@
 const SideBar = () => {
   return(
-    <div>Sidebar</div>
+    <div className="shadow-2xl p-5 w-48 font-serif">
+        <ul>
+          <li>Home </li>
+          <li>Shorts</li>
+          <li>Videos</li>
+          <li>Live</li>
+        </ul>
+      
+        <h2 className="font-bold pt-5">Subscriptions</h2>
+        <ul className="text-sm">
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+        
+        <h2 className="font-bold pt-5">Watch Later</h2>
+        <ul className="text-sm">
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+    </div>
   )
 }
 
