10ce3df40fbc62902c556bffdbeb12fa9af676d3
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-05-14 23:44:46 +0530

    provided store to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Head from "./components/Head";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "../utils/store"
 
 function App() {
   return (
-    <div>
-      <Head/>
-      <Body/>
+    <Provider store={store}>
+      <div>
+        <Head/>
+        <Body/>
     </div>
+    </Provider>
   )
 }
 
