e5b15889de0ab6dcef38c52bdcedaee638effaf8
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-06-20 00:09:13 +0530

    Destructuring name prop and passing it to the component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
-const Button = () => {
+const Button = ({name}:{name:string}) => {
   return(
     <div> 
-      <button className="m-2 px-2 bg-gray-300 rounded-md"> All </button>
+      <button className="m-2 px-2 bg-gray-300 rounded-md"> {name} </button>
     </div>
   )
 }
--- src/components/ButtonList.tsx
+++ src/components/ButtonList.tsx
@@ -3,12 +3,12 @@ import Button from "./Button";
 const ButtonList = () => {
   return (
     <div className="flex">
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button name='All' />
+      <Button name='Cricket'/>
+      <Button name='Movies' />
+      <Button name='Kapil Sharma'/>
+      <Button name='Diljit' />
+      <Button name='Zakir'/>
     </div>
   )
 }
