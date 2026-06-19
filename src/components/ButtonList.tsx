539820baa74b43829ab9da9b90a090f76d6c2be7
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-06-20 00:47:05 +0530

    feat: scroll ButtonList

--- src/components/ButtonList.tsx
+++ src/components/ButtonList.tsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
 
-const categories = ["All", "Cricket","Movies","News", "Podcasts"]
+const categories = ["All", "Cricket","Movies","News", "Podcasts","All", "Cricket","Movies","News", "Podcasts","All", "Cricket","Movies","News", "Podcasts","All", "Cricket","Movies","News", "Podcasts"]
 
 const ButtonList = () => {
   return (
-    <div className="flex">
+    <div className="flex overflow-x-auto max-w-full">
       {
         categories.map((category , index) => (
           <Button key={index} name={category}/>
